37987b7f5111d4156f2b14c5edbf7696020f6168
vkuweb <106548035+vkuweb@users.noreply.github.com>
2025-12-03 13:21:39 +0700

    helloSpeaker.js

---
+++ starter/helloSpeaker.js
@@ -0,0 +1,9 @@
+(function (window) {
+  var helloSpeaker = {};
+
+  helloSpeaker.speak = function (name) {
+    console.log("Hello " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
